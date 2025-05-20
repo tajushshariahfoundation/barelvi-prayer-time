045cc93c30daa48b4141d415a7f18b33b2adbdeb
Bondugula Aadhi <aadhibondugula@gmail.com>
2025-05-20 19:12:17 +0530

    commit into main branch

---
+++ src/Prayer.ts
@@ -0,0 +1,12 @@
+const Prayer = {
+  Fajr: "fajr",
+  Sunrise: "sunrise",
+  Dhuhr: "dhuhr",
+  Asr: "asr",
+  Maghrib: "maghrib",
+  Isha: "isha",
+  Dahwa: "dahwa",
+  None: "none",
+} as const;
+
+export default Prayer;
